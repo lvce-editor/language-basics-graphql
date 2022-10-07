9e46f4f56999938908de59bf2380a16fc396ae06
Le Vivilet <levivlet@pm.me>
2022-10-07 11:58:49 +0200

    add highlighting for comment

--- src/tokenizeGraphql.js
+++ src/tokenizeGraphql.js
@@ -1,28 +1,66 @@
+const State = {
+  TopLevelContent: 1,
+}
+
 /**
  * @enum number
  */
 export const TokenType = {
+  None: 0,
   Text: 1,
+  Comment: 2,
 }
 
 export const TokenMap = {
   [TokenType.Text]: 'Text',
+  [TokenType.Comment]: 'Comment',
 }
 
 export const initialLineState = {
-  state: 1,
+  state: State.TopLevelContent,
   tokens: [],
 }
+
+const RE_LINE_COMMENT = /^#.*/s
+const RE_ANYTHING = /^.+/s
 
 export const hasArrayReturn = true
 
 /**
  * @param {string} line
+ * @param {any} lineState
  */
-export const tokenizeLine = (line) => {
-  const tokens = [TokenType.Text, line.length]
+export const tokenizeLine = (line, lineState) => {
+  let next = null
+  let index = 0
+  let tokens = []
+  let token = TokenType.None
+  let state = lineState.state
+  while (index < line.length) {
+    const part = line.slice(index)
+    switch (state) {
+      case State.TopLevelContent:
+        if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.Text
+          state = State.TopLevelContent
+        } else {
+          part //?
+          throw new Error('no')
+        }
+        break
+
+      default:
+        throw new Error('no')
+    }
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
+  }
   return {
-    state: 1,
+    state,
     tokens,
   }
 }
