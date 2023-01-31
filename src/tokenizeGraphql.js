3d1ab89263f1cdd609cd863707b62d7efd325bc1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-31 21:59:12 +0100

    fix: basic highlighting of properties and functions (#11)

--- src/tokenizeGraphql.js
+++ src/tokenizeGraphql.js
@@ -1,6 +1,10 @@
 const State = {
   TopLevelContent: 1,
   AfterKeywordType: 2,
+  AfterTypeName: 3,
+  InsideTypeObject: 4,
+  InsideFunctionParameters: 6,
+  BeforeType: 7,
 }
 
 /**
@@ -13,6 +17,9 @@ export const TokenType = {
   Whitespace: 3,
   Keyword: 4,
   Type: 5,
+  Punctuation: 6,
+  VariableName: 7,
+  Function: 8,
 }
 
 export const TokenMap = {
@@ -21,10 +28,14 @@ export const TokenMap = {
   [TokenType.Whitespace]: 'Whitespace',
   [TokenType.Keyword]: 'Keyword',
   [TokenType.Type]: 'Type',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.VariableName]: 'VariableName',
+  [TokenType.Function]: 'Function',
 }
 
 export const initialLineState = {
   state: State.TopLevelContent,
+  stack: [],
   tokens: [],
 }
 
@@ -33,6 +44,15 @@ const RE_ANYTHING = /^.+/s
 const RE_KEYWORD = /^(?:type|scalar|enum)\b/
 const RE_WHITESPACE = /^\s+/
 const RE_VARIABLE_NAME = /^[a-zA-Z][a-zA-Z\d\_\-]*/
+const RE_CURLY_OPEN = /^\{/
+const RE_ROUND_OPEN = /^\(/
+const RE_ROUND_CLOSE = /^\)/
+const RE_CURLY_CLOSE = /^\}/
+const RE_COLON = /^\:/
+const RE_FUNCTION_CALL_NAME = /^[\w]+(?=\s*\()/
+const RE_EXCLAMATION_MARK = /^\!/
+const RE_SQUARE_OPEN = /^\[/
+const RE_SQUARE_CLOSE = /^\]/
 
 export const hasArrayReturn = true
 
@@ -46,6 +66,7 @@ export const tokenizeLine = (line, lineState) => {
   let tokens = []
   let token = TokenType.None
   let state = lineState.state
+  let stack = [...lineState.stack]
   while (index < line.length) {
     const part = line.slice(index)
     switch (state) {
@@ -80,7 +101,7 @@ export const tokenizeLine = (line, lineState) => {
           state = State.AfterKeywordType
         } else if ((next = part.match(RE_VARIABLE_NAME))) {
           token = TokenType.Type
-          state = State.TopLevelContent
+          state = State.AfterTypeName
         } else if ((next = part.match(RE_LINE_COMMENT))) {
           token = TokenType.Comment
           state = State.TopLevelContent
@@ -88,7 +109,99 @@ export const tokenizeLine = (line, lineState) => {
           throw new Error('no')
         }
         break
-
+      case State.AfterTypeName:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.AfterTypeName
+        } else if ((next = part.match(RE_CURLY_OPEN))) {
+          token = TokenType.Punctuation
+          state = State.InsideTypeObject
+        } else {
+          throw new Error('no')
+        }
+        break
+      case State.InsideTypeObject:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.InsideTypeObject
+        } else if ((next = part.match(RE_FUNCTION_CALL_NAME))) {
+          token = TokenType.Function
+          state = State.InsideTypeObject
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.VariableName
+          state = State.InsideTypeObject
+        } else if ((next = part.match(RE_COLON))) {
+          token = TokenType.Punctuation
+          state = State.BeforeType
+        } else if ((next = part.match(RE_ROUND_OPEN))) {
+          token = TokenType.Punctuation
+          stack.push(State.InsideTypeObject)
+          state = State.InsideFunctionParameters
+        } else if ((next = part.match(RE_CURLY_CLOSE))) {
+          token = TokenType.Punctuation
+          state = stack.pop() || State.TopLevelContent
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.InsideTypeObject
+        } else if ((next = part.match(RE_ROUND_CLOSE))) {
+          token = TokenType.Punctuation
+          state = State.InsideTypeObject
+        } else {
+          part
+          throw new Error('no')
+        }
+        break
+      case State.InsideFunctionParameters:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.InsideFunctionParameters
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.VariableName
+          state = State.InsideFunctionParameters
+        } else if ((next = part.match(RE_COLON))) {
+          token = TokenType.Punctuation
+          state = State.BeforeType
+        } else if ((next = part.match(RE_ROUND_CLOSE))) {
+          token = TokenType.Punctuation
+          state = stack.pop() || State.TopLevelContent
+        } else if ((next = part.match(RE_CURLY_CLOSE))) {
+          token = TokenType.Punctuation
+          state = stack.pop() || State.TopLevelContent
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.InsideFunctionParameters
+        } else {
+          part
+          throw new Error('no')
+        }
+        break
+      case State.BeforeType:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.BeforeType
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.Type
+          state = State.BeforeType
+        } else if ((next = part.match(RE_ROUND_CLOSE))) {
+          token = TokenType.Punctuation
+          state = stack.pop() || State.TopLevelContent
+        } else if ((next = part.match(RE_EXCLAMATION_MARK))) {
+          token = TokenType.Punctuation
+          state = State.BeforeType
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.BeforeType
+        } else if ((next = part.match(RE_SQUARE_OPEN))) {
+          token = TokenType.Punctuation
+          state = State.BeforeType
+        } else if ((next = part.match(RE_SQUARE_CLOSE))) {
+          token = TokenType.Punctuation
+          state = State.BeforeType
+        } else {
+          part
+          throw new Error('no')
+        }
+        break
       default:
         throw new Error('no')
     }
@@ -96,8 +209,12 @@ export const tokenizeLine = (line, lineState) => {
     index += tokenLength
     tokens.push(token, tokenLength)
   }
+  if (state === State.BeforeType) {
+    state = State.InsideTypeObject
+  }
   return {
     state,
+    stack,
     tokens,
   }
 }
